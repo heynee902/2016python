ad05cdd51bbbc05c0afa789256eff05cad72708d
heynee902 <zotehr12@naver.com>
2016-05-30 21:19:19 +0900

    영화 코드부분 제대로 출력되게
    초기화 문제였음

--- _2016PythonVisual.py
+++ _2016PythonVisual.py
@@ -1,181 +1,147 @@
 # -*- coding: utf-8 -*-
-from xml.dom.minidom import parse, parseString
 from xml.etree import ElementTree
-
-import sys
-import urllib
-import time
-
-import sys
-from PyQt5 import QtWidgets
-
-import urllib
-import http.client
-
-
+from http.client import HTTPConnection
 loopFlag = 1
 xmlFD = -1
-BooksDoc = None
-
 ##global
 conn = None
 regKey = 'bb48880c629b0289e1b1fea9d38c09a5'
-# ¹Ú½º¿ÀÇÇ½º OpenAPI Á¢¼Ó Á¤º¸ information
+# ë°•ìŠ¤ì˜¤í”¼ìŠ¤ OpenAPI ì ‘ì† ì •ë³´ information
 server = "kobis.or.kr"
 itemElements = None
 
+#####ì˜í™”ì½”ë“œ ì €ìž¥ ë¦¬ìŠ¤íŠ¸
+movieTop10 = []
+movieTop10d = dict()
 
-def main():
-    app = QtWidgets.QApplication(sys.argv)
-    window = QtWidgets.QMainWindow()
-    button = QtWidgets.QPushButton("Hello world")
-    window.setCentralWidget(button)
-    window.show()
-    app.exec_()
 
-if __name__ == '__main__':
-    main()
+def userURIBuilder(server,**user):  #**ì€ ì‚¬ì „í˜•ì‹ìœ¼ë¡œ ë°˜í™˜í•´ì£¼ëŠ”
+    str = "http://" + server + "/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.xml" + "?"
+    for key in user.keys():
+        str += key + "=" + user[key] + "&"
+    return str
+
+def connectOpenAPIServer():
+    global conn, server
+    conn = HTTPConnection(server)
+    
+def getBookDataFromISBN(date):
+    global server, regKey, conn, req
+    if conn == None :
+        connectOpenAPIServer()
+    uri = userURIBuilder(server, key=regKey, targetDt=date)
+    conn.request("GET", uri)
+
+    req = conn.getresponse()
+    print (req.status)
+    if int(req.status) == 200 :
+        print("BoxOffice data downloading complete!")
+        extractBookData(req.read())
+    else:
+        print("OpenAPI request has been failed!! please retry")
+        return None
+
+def extractBookData(strXml):
+    global itemElements    
+ 
+    
+    tree = ElementTree.fromstring(strXml)
+    itemElements = tree.getiterator("dailyBoxOffice")  # return list type
+    movieTop10.clear()
+    for item in itemElements:
+        date = item.find("rank")
+        strTitle = item.find("movieNm")
+        movieCd = item.find("movieCd").text     #movieCdë¥¼ ì°¾ì•„ì„œ movieCdì—ë„£ì–´ì¤Œ
+        movieTop10.append(movieCd)      #ë¦¬ìŠ¤íŠ¸ì— ë„£ëŠ”ë‹¤
+        
+        if len(strTitle.text) > 0 :
+           print(date.text,"ìœ„ : ",strTitle.text)
+          
+    for x in range(10): #ì‚¬ì „ì— ë¦¬ìŠ¤íŠ¸ ë„£ì–´ì£¼ê¸°
+        movieTop10d[x+1] = movieTop10[x]
+        
+    print("\n-----------------------------------------\n")
+    for k,v in movieTop10d.items():
+        print(k,"ìœ„ Code : ",v)     #ì‹œí—˜ ì¶œë ¥
 
 
 #### Menu  implementation
 def printMenu():
-	print("\nWelcome! BoxOffice Movie Manager Program (xml version)") 
-	print("========Menu==========")
-	print("Quit program: q")
-	print("print movie list: p")
-	print("sEarch Daily BoxOffice list: e")
-	print("Search movieCd: c")
-	print("==================")
-	
-def userURIBuilder(server,**user):  #**Àº »çÀüÇü½ÄÀ¸·Î ¹ÝÈ¯?ÇØÁÖ´Â
-	str = "http://" + server + "/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.xml" + "?"
-	for key in user.keys():
-		str += key + "=" + user[key] + "&"
-	return str
-
-
-def connectOpenAPIServer():
-	global conn, server
-	conn = HTTPConnection(server)
-
-def API(server, url):
-    conn = http.client.HTTPConnection(server)
-    conn.request("GET",url)
-    req = conn.getresponse()
-    req.status
-    req.reason
-    return req.read().decode('utf-8')
-
-
-def getBookDataFromISBN(date):
-	global server, regKey, conn, req
-	if conn == None :
-		connectOpenAPIServer()
-	uri = userURIBuilder(server, key=regKey, targetDt=date)
-	conn.request("GET", uri)
-
-	req = conn.getresponse()
-	print (req.status)
-	if int(req.status) == 200 :
-		print("BoxOffice data downloading complete!")
-		extractBookData(req.read())
-	else:
-		print("OpenAPI request has been failed!! please retry")
-		return None
-
-
-def extractBookData(strXml):
-	global itemElements
-	tree = ElementTree.fromstring(strXml)
-	itemElements = tree.getiterator("dailyBoxOffice")  # return list type
-	for item in itemElements:
-		date = item.find("rank")
-		strTitle = item.find("movieNm")
-		if len(strTitle.text) > 0 :
-			print(date.text,"À§ : ", strTitle.text)
-
-
+    print("\nWelcome! BoxOffice Movie Manager Program (xml version)") 
+    print("========Menu==========")
+    print("Quit program: q")
+    print("print movie list: p")
+    print("sEarch Daily BoxOffice list: e")
+    print("Search movieCd: c")
+    print("==================")
+    
 def launcherFunction(menu):
-	global BooksDoc  
-	global itemElements
-
-	if menu == 'e':
-		date = str(input ('Á¶È¸ÇÒ ³¯Â¥ ÀÔ·Â(yyyymmdd):'))
-		getBookDataFromISBN(date)
-	  #  printBookList(SearchBookTitle(keyword))
-#    elif menu == 'p':
-#        print(itemElements)
-	elif menu == 'c':
-		keyword = str(input ('input keyword to search :'))
-		printBookList(SearchBookTitle(keyword)) 
-	else:
-		print ("error : unknow menu key")
-
- 
+    global itemElements
+    if menu == 'e':
+        date = str(input ('ì¡°íšŒí•  ë‚ ì§œ ìž…ë ¥(yyyymmdd):'))
+        getBookDataFromISBN(date)
+      #  printBookList(SearchBookTitle(keyword))
+    elif menu == 'p':
+        print(itemElements)
+    elif menu == 'c':
+        keyword = str(input ('input keyword to search :'))
+        SearchBookTitle(keyword)
+    elif menu == 'q':
+        QuitBookMgr()
+    else:
+        print ("error : unknow menu key")        
+        
+    
+     
 def PrintBookList(tags):
-	global BooksDoc
+    global BooksDoc
 #    if not checkDocument():
  #      return None
-    
-	booklist = BooksDoc.childNodes
-	book = booklist[0].childNodes
-	for item in book:
-		if item.nodeName == "dailyBoxOffice":
-			subitems = item.childNodes
-			for atom in subitems:
-			   if atom.nodeName in tags:
-				   print("title=",atom.firstChild.nodeValue)
-               
-               
+        
+    booklist = BooksDoc.childNodes
+    book = booklist[0].childNodes
+    for item in book:
+        if item.nodeName == "dailyBoxOffice":
+            subitems = item.childNodes
+            for atom in subitems:
+               if atom.nodeName in tags:
+                   print("title=",atom.firstChild.nodeValue)
+                   
+                   
 def SearchBookTitle(keyword):
-	global BooksDoc
-	retlist = []
+    global BooksDoc
 #    if not checkDocument():
  #       return None
+    retlist = []
+        
+    try:
+        tree = ElementTree.fromstring(str(BooksDoc.toxml()))
+    except Exception:
+        print ("Element Tree parsing Error : maybe the xml document is not corrected.")
+        return None
     
-	try:
-		tree = ElementTree.fromstring(str(BooksDoc.toxml()))
-	except Exception:
-		print ("Element Tree parsing Error : maybe the xml document is not corrected.")
-		return None
-
-	itemElements = tree.getiterator("movieNm")  # return list type
-	for item in itemElements:
-		strTitle = item.find("movieNm")
-		if (strTitle.text.find(keyword) >=0 ):
-			retlist.append((item.attrib["movieCd"], strTitle.text))
-
-	return retlist
-
-
-               
+    itemElements = tree.getiterator(keyword)  # return list type
+    for item in itemElements:
+        strTitle = item.find("movieNm").text
+        if len(strTitle.text) > 0 :
+           print(strTitle.text,": ",item.attrib["movieCd"].text)
+    return retlist
+    
+    
+                   
 def printBookList(retlist):
-	for res in retlist:
-		print (res)
-    
-    
-		"""
-def checkDocument():
-	global BooksDoc
-	if BooksDoc == None:
-		print("Error : Document is empty")
-		return False
-	return True
-    
-		"""
-    
+    for res in retlist:
+        print (res)
+        
+        
+def QuitBookMgr():
+    global loopFlag
+    loopFlag = 0
     
 ##### run #####
 while(loopFlag > 0):
-	printMenu()
-	menuKey = str(input ('select menu :'))
-	launcherFunction(menuKey)
+    printMenu()
+    menuKey = str(input ('select menu :'))
+    launcherFunction(menuKey)
 else:
-	print ("Thank you! Good Bye")
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    form = MainWindow()
-    form.setWindowIcon(QIcon('./Img/main_icon.png'))
-    form.show()
-    sys.exit(app.exec_())
+    print ("Thank you! Good Bye")
