12a7375406ad1b78e330ea662a62801dd8aa2b39
heynee902 <zotehr12@naver.com>
2016-05-31 02:28:48 +0900

    GUI 연동, 일단 창만 뜸
    GUI 연동 코드랑 매칭 필요

--- _2016PythonVisual.py
+++ _2016PythonVisual.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 from xml.etree import ElementTree
 from http.client import HTTPConnection
+
+import sys
+import GUI
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QGridLayout, QLabel, QLineEdit
+from PyQt5.QtWidgets import QTextEdit, QWidget, QDialog, QApplication
+from PyQt5 import QtCore, QtGui, QtWidgets
+
 loopFlag = 1
 xmlFD = -1
 ##global
@@ -15,133 +24,119 @@ movieTop10 = []
 movieTop10d = dict()
 
 
-def userURIBuilder(server,**user):  #**은 사전형식으로 반환해주는
-    str = "http://" + server + "/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.xml" + "?"
-    for key in user.keys():
-        str += key + "=" + user[key] + "&"
-    return str
+class MainWindow(QDialog, GUI.Ui_Dialog):
+	def __init__(self, parent=None):
+		super(MainWindow, self).__init__(parent)
+		self.setupUi(self)
 
-def connectOpenAPIServer():
-    global conn, server
-    conn = HTTPConnection(server)
-    
-def getBookDataFromISBN(date):
-    global server, regKey, conn, req
-    if conn == None :
-        connectOpenAPIServer()
-    uri = userURIBuilder(server, key=regKey, targetDt=date)
-    conn.request("GET", uri)
+	def userURIBuilder(server,**user):  #**은 사전형식으로 반환해주는
+		str = "http://" + server + "/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.xml" + "?"
+		for key in user.keys():
+			str += key + "=" + user[key] + "&"
+		return str
 
-    req = conn.getresponse()
-    print (req.status)
-    if int(req.status) == 200 :
-        print("BoxOffice data downloading complete!")
-        extractBookData(req.read())
-    else:
-        print("OpenAPI request has been failed!! please retry")
-        return None
+	def connectOpenAPIServer():
+		global conn, server
+		conn = HTTPConnection(server)
 
-def extractBookData(strXml):
-    global itemElements    
- 
-    
-    tree = ElementTree.fromstring(strXml)
-    itemElements = tree.getiterator("dailyBoxOffice")  # return list type
-    movieTop10.clear()
-    for item in itemElements:
-        date = item.find("rank")
-        strTitle = item.find("movieNm")
-        movieCd = item.find("movieCd").text     #movieCd를 찾아서 movieCd에넣어줌
-        movieTop10.append(movieCd)      #리스트에 넣는다
-        
-        if len(strTitle.text) > 0 :
-           print(date.text,"위 : ",strTitle.text)
-          
-    for x in range(10): #사전에 리스트 넣어주기
-        movieTop10d[x+1] = movieTop10[x]
-        
-    print("\n-----------------------------------------\n")
-    for k,v in movieTop10d.items():
-        print(k,"위 Code : ",v)     #시험 출력
+	def getBookDataFromISBN(date):
+		global server, regKey, conn, req
+		if conn == None :
+			connectOpenAPIServer()
+		uri = userURIBuilder(server, key=regKey, targetDt=date)
+		conn.request("GET", uri)
+
+		req = conn.getresponse()
+		print (req.status)
+		if int(req.status) == 200 :
+			print("BoxOffice data downloading complete!")
+			extractBookData(req.read())
+		else:
+			print("OpenAPI request has been failed!! please retry")
+			return None
+
+	def extractBookData(strXml):
+		global itemElements    
+		tree = ElementTree.fromstring(strXml)
+		itemElements = tree.getiterator("dailyBoxOffice")  # return list type
+		movieTop10.clear()
+		for item in itemElements:
+			date = item.find("rank")
+			strTitle = item.find("movieNm")
+			movieCd = item.find("movieCd").text     #movieCd를 찾아서 movieCd에넣어줌
+			movieTop10.append(movieCd)      #리스트에 넣는다
+			if len(strTitle.text) > 0 :
+			   print(date.text,"위 : ",strTitle.text)
+		for x in range(10): #사전에 리스트 넣어주기
+			movieTop10d[x+1] = movieTop10[x]
+		print("\n-----------------------------------------\n")
+		for k,v in movieTop10d.items():
+			print(k,"위 Code : ",v)     #시험 출력
 
 
-#### Menu  implementation
-def printMenu():
-    print("\nWelcome! BoxOffice Movie Manager Program (xml version)") 
-    print("========Menu==========")
-    print("Quit program: q")
-    print("print movie list: p")
-    print("sEarch Daily BoxOffice list: e")
-    print("Search movieCd: c")
-    print("==================")
-    
-def launcherFunction(menu):
-    global itemElements
-    if menu == 'e':
-        date = str(input ('조회할 날짜 입력(yyyymmdd):'))
-        getBookDataFromISBN(date)
-      #  printBookList(SearchBookTitle(keyword))
-    elif menu == 'p':
-        print(itemElements)
-    elif menu == 'c':
-        keyword = str(input ('input keyword to search :'))
-        SearchBookTitle(keyword)
-    elif menu == 'q':
-        QuitBookMgr()
-    else:
-        print ("error : unknow menu key")        
-        
-    
-     
-def PrintBookList(tags):
-    global BooksDoc
-#    if not checkDocument():
- #      return None
-        
-    booklist = BooksDoc.childNodes
-    book = booklist[0].childNodes
-    for item in book:
-        if item.nodeName == "dailyBoxOffice":
-            subitems = item.childNodes
-            for atom in subitems:
-               if atom.nodeName in tags:
-                   print("title=",atom.firstChild.nodeValue)
-                   
-                   
-def SearchBookTitle(keyword):
-    global BooksDoc
-#    if not checkDocument():
- #       return None
-    retlist = []
-        
-    try:
-        tree = ElementTree.fromstring(str(BooksDoc.toxml()))
-    except Exception:
-        print ("Element Tree parsing Error : maybe the xml document is not corrected.")
-        return None
-    
-    itemElements = tree.getiterator(keyword)  # return list type
-    for item in itemElements:
-        strTitle = item.find("movieNm").text
-        if len(strTitle.text) > 0 :
-           print(strTitle.text,": ",item.attrib["movieCd"].text)
-    return retlist
-    
-    
-                   
-def printBookList(retlist):
-    for res in retlist:
-        print (res)
-        
-        
-def QuitBookMgr():
-    global loopFlag
-    loopFlag = 0
-    
-##### run #####
-while(loopFlag > 0):
-    printMenu()
-    menuKey = str(input ('select menu :'))
-    launcherFunction(menuKey)
-else:
-    print ("Thank you! Good Bye")
+	#### Menu  implementation
+	def printMenu():
+		print("\nWelcome! BoxOffice Movie Manager Program (xml version)") 
+		print("========Menu==========")
+		print("Quit program: q")
+		print("print movie list: p")
+		print("sEarch Daily BoxOffice list: e")
+		print("Search movieCd: c")
+		print("==================")
+
+	def launcherFunction(menu):
+
+		global itemElements
+
+		if menu == 'e':
+			date = str(input ('조회할 날짜 입력(yyyymmdd):'))
+			getBookDataFromISBN(date)
+		  #  printBookList(SearchBookTitle(keyword))
+		elif menu == 'p':
+			print(itemElements)
+		elif menu == 'c':
+			keyword = str(input ('input keyword to search :'))
+			SearchBookTitle(keyword)
+		elif menu == 'q':
+			QuitBookMgr()
+		else:
+			print ("error : unknow menu key")        
+
+	def SearchBookTitle(keyword):
+		global BooksDoc
+	#    if not checkDocument():
+	 #       return None
+		retlist = []
+		try:
+			tree = ElementTree.fromstring(str(BooksDoc.toxml()))
+		except Exception:
+			print ("Element Tree parsing Error : maybe the xml document is not corrected.")
+			return None
+	
+		itemElements = tree.getiterator(keyword)  # return list type
+		for item in itemElements:
+			strTitle = item.find("movieNm").text
+			if len(strTitle.text) > 0 :
+			   print(strTitle.text,": ",item.attrib["movieCd"].text)
+		return retlist
+	
+	def printBookList(retlist):
+		for res in retlist:
+			print (res)
+
+	def QuitBookMgr():
+		global loopFlag
+		loopFlag = 0
+		##### run #####
+		while(loopFlag > 0):
+			printMenu()
+			menuKey = str(input ('select menu :'))
+			launcherFunction(menuKey)
+		else:
+			print ("Thank you! Good Bye")
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	form = MainWindow()
+	form.show()
+	sys.exit(app.exec_())
